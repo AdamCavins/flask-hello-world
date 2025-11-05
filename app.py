cb007f5b52b5e2b8c5c813ed581acb6c70aea5cb
Adam Cavins <adamdcavins@gmail.com>
2025-11-05 19:49:00 +0000

    db test #2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello_world():
     return 'Hello, World from Adam Cavins in 3308'
 
 @app.route('/db_test')
-def db_text():
+def testing():
     conn = psycopg2.connect("postgresql://lab_10_postgres_example_user:6B9Mk6jzeAc7heFuPwWaga9xq7PEnsVw@dpg-d45qa6s9c44c73c9iefg-a/lab_10_postgres_example")
     conn.close()
     return "Database Connection Successful"
